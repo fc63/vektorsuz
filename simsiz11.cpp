41a21eb9533d7018798989bd48836d65edb350d0
Furkan Çoban <139001749+fc63@users.noreply.github.com>
2023-12-23 13:09:06 +0300

    Add files via upload

---
+++ simsiz11.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <string>
+
+class Ogrenci {
+	private:
+		std::string isim,soyisim;
+		std::string* dersadi;
+		std::string* dersadiupt;
+		static int derscount;
+		int numarasi;
+	public:
+		Ogrenci(std::string isim,std::string soyisim, int numarasi){
+			this->isim=isim;
+			this->soyisim=soyisim;
+			this->numarasi=numarasi;
+			std::cout<<this->numarasi<<" Numarali "<<this->isim<<" , "<<this->soyisim<<" eklenmistir";
+		}
+		Ogrenci(){
+			std::cout<<"\nDegisken olusturuldu\n";
+		}
+		~Ogrenci(){
+			std::cout<<"\nDest\n";
+		}
+		std::string getisim()const{
+			return isim;
+		}
+		std::string getsoyisim()const{
+			return soyisim;
+		}
+		int getno()const{
+			return numarasi;
+		}
+		
+};
+
+int main(){
+	int choice=0,numara,ogrencisayaci=0,kapasite=2,i=0,tempi=0,j=0;
+	Ogrenci* ogrenci=new Ogrenci[kapasite];
+	std::string ad,soyad;
+	do{
+		std::cout<<"\nOgrenci Not Sistemi\n1.Ogrenci Ekle\n2.Ogrenci Sil\n3.Ogrenci Bilgileri\n4.Kapat\nSeciminiz: ";
+		std::cin>>choice;
+		switch(choice){
+			
+			case 1:
+				std::cout<<"\nOgrenci Adi :";
+				std::cin>>ad;
+				std::cout<<"\nOgrenci Soyadi :";
+				std::cin>>soyad;
+				std::cout<<"\nOgrenci Numarasi :";
+				std::cin>>numara;
+				if(kapasite>ogrencisayaci){
+					ogrenci[ogrencisayaci++]={ad,soyad,numara};
+				}
+				else{
+					Ogrenci* temp=new Ogrenci[++kapasite];
+					for(i=0;i<ogrencisayaci;i++){
+						temp[i]=ogrenci[i];
+					}
+					delete[] ogrenci;
+					ogrenci=temp;
+					std::cout<<"\nkapasite artirildi\n";
+					ogrenci[ogrencisayaci++]={ad,soyad,numara};
+				}
+				break;
+				
+				
+				case 2:
+					{
+					
+					for(i=0;i<ogrencisayaci;i++){
+						std::cout<<i<<". "<<ogrenci[i].getisim()<<" "<<ogrenci[i].getsoyisim()<<" "<<ogrenci[i].getno()<<"\n";
+					}
+					std::cout<<"\nSeciminiz :";
+					std::cin>>tempi;
+					Ogrenci* temp=new Ogrenci[--kapasite];
+					for(i=0;i<tempi;i++){
+						temp[i]=ogrenci[i];
+					}
+					for(i=tempi;i<--ogrencisayaci;i++){
+						temp[i]=ogrenci[i+1];
+					}
+					delete[] ogrenci;
+					ogrenci=temp;
+					delete[] temp;
+					temp=nullptr;
+					std::cout<<"\nOgrenci Silindi ve kapasite dusuruldu\n";
+				}
+				case 3:	
+						for(i=0;i<ogrencisayaci;i++){
+						std::cout<<i<<" . "<<ogrenci[i].getisim()<<" "<<ogrenci[i].getsoyisim()<<" "<<ogrenci[i].getno()<<"\n";
+					}
+				default:
+					std::cout<<"Yanlis Giris";
+					break;
+
+}
+
+}while(choice!=4);
+	delete[] ogrenci;
+	ogrenci=nullptr;
+	return 0;
+}
